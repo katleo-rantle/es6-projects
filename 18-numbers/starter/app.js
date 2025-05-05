8350bf5c19e3ad0d2b9b0910857269dbaf4e308d
katleo rantle <kprantle@gmal.com>
2025-05-05 09:20:24 +0200

    project 18 complete

--- 18-numbers/starter/app.js
+++ 18-numbers/starter/app.js
@@ -1,1 +1,19 @@
-console.log('numbers project');
+const numbers = [...document.querySelectorAll('.number')];
+
+const updateCount = (el) => {
+  const value = parseInt(el.dataset.value);
+  const incrementValue = Math.ceil(value / 1000);
+  let startValue = 0;
+
+  const increaseCount = setInterval(() => {
+    startValue += incrementValue;
+    if (startValue >= value) {
+      el.textContent = `${value}+`;
+      clearInterval(increaseCount);
+      return;
+    }
+    el.textContent = `${startValue}+`;
+  }, 10);
+};
+
+numbers.forEach((number) => updateCount(number));
